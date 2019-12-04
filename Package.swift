70abcfeda4ca85d1d0460ee8a485b93479062fc6
Christoph Duyster <christoph.duyster@icloud.com>
2019-12-04 17:37:27 +0100

    package

--- Package.swift
+++ Package.swift
@@ -4,25 +4,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "SFSymbolsEnum",
-    platforms: [
-        .iOS(.v8),
-        .tvOS(.v9),
-        .watchOS(.v2)
-    ],
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "SFSymbolsEnum",
-            targets: ["SFSymbolsEnum"])
+    name: "SFSymbols",
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "SFSymbolsEnum",
-            dependencies: [],
-            path: "SFSymbolsEnum")
-    ],
-    swiftLanguageVersions: [.v5]
+            name: "SFSymbols",
+            dependencies: []),
+        .testTarget(
+            name: "SFSymbolsTests",
+            dependencies: ["SFSymbols"]),
+    ]
 )
