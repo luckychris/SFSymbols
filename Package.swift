81a2f264e9c92f3431600a8e5601a27f7bf46918
Christoph <lucky_chris_again@yahoo.de>
2019-12-05 06:13:51 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SFSymbols",
-            dependencies: ["UIKit"]),
+            dependencies: []),
         .testTarget(
             name: "SFSymbolsTests",
             dependencies: ["SFSymbols"]),
